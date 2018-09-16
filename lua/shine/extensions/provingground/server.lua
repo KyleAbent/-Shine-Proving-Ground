1c7ca868338d88d1da00bfb00647d411a701e326
Kyle <kyleabent@gmail.com>
2018-09-15 23:46:49 -0400

    9.15.18 pt 2

--- lua/shine/extensions/provingground/server.lua
+++ lua/shine/extensions/provingground/server.lua
@@ -111,7 +111,7 @@ end
                       end
                    end
         if lowestPriority == 999 then return end
-        Shared.ConsoleCommand(string.format("sh_setteam %s 1", self.marineQueue[index].steamID )) 
+        if self.marineQueue[index].plyr:GetControllingPlayer():GetTeamNumber() == 0 then  Shared.ConsoleCommand(string.format("sh_setteam %s 1", self.marineQueue[index].steamID ))  end
         Shine:NotifyDualColour( self.marineQueue[index].plyr, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "Congratulations! You're the priority in the Marine Team Queue!" )
         Shine:NotifyDualColour( self.marineQueue[index].plyr, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "You've been removed from the Marine Team Queue" )
         marinePlaceInQueue = marinePlaceInQueue - 1 
@@ -128,7 +128,7 @@ end
                       end
                    end
         if lowestPriority == 999 then return end      
-        Shared.ConsoleCommand(string.format("sh_setteam %s 2", self.alienQueue[index].steamID ))
+        if self.alienQueue[index].plyr:GetControllingPlayer():GetTeamNumber() == 1 then  Shared.ConsoleCommand(string.format("sh_setteam %s 2", self.alienQueue[index].steamID ))
         Shine:NotifyDualColour( self.alienQueue[index].plyr, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "Congratulations! You're the priority in the Alien Team Queue!" )
         Shine:NotifyDualColour( self.alienQueue[index].plyr, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "You've been removed from the Alien Team Queue" )
         alienPlaceInQueue = alienPlaceInQueue - 1
@@ -145,7 +145,7 @@ end
                       end
                    end
         if lowestPriority == 999 then return end               
-        Shared.ConsoleCommand(string.format("sh_setteam %s 3", self.spectQueue[index].steamID ))
+        if self.spectQueue[index].plyr:GetControllingPlayer():GetTeamNumber() == 0 then  Shared.ConsoleCommand(string.format("sh_setteam %s 3", self.spectQueue[index].steamID ))
         Shine:NotifyDualColour( self.spectQueue[index].plyr, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "Congratulations! You're the priority in the Spectate Team Queue!" )
         Shine:NotifyDualColour( self.spectQueue[index].plyr, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "You've been removed from the Spectator Team Queue" )
         spectPlaceInQueue = spectPlaceInQueue - 1
@@ -170,41 +170,54 @@ function Plugin:JoinTeam(gamerules, player, newteam, force, ShineForce)
     if ShineForce or newteam == kSpectatorIndex or newteam == kTeamReadyRoom then return end
      local Client = player:GetClient()
     --self:checkAndClearQueue(player, newteam)
+    
+    local AlienCount = gamerules:GetTeam2():GetNumPlayers()
+    local MarineCount = gamerules:GetTeam1():GetNumPlayers()
+    local SpectCount = gamerules:GetTeam3():GetNumPlayers()
+    
+    if MarineCount < 12 then
     self:findNextPriority(1)
+    end
+     
+    if AlienCount < 12 then
     self:findNextPriority(2)
+    end
+      
+    if SpectCount < 6 then
     self:findNextPriority(3)
+    end
  
     if newteam == 2 then
-    local AlienCount = gamerules:GetTeam2():GetNumPlayers()
+
        if AlienCount >= 12 then
         Shine:NotifyDualColour( player, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "Alien Team Capped at 12 players" )
         Shine:NotifyDualColour( player, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "You've entered the Alien Team Queue at number " .. alienPlaceInQueue) ---Great, what pos in queueue are we?
         self:RemoveFromOtherQueue(player, newteam)
-        self.alienQueue[ alienPlaceInQueue ] = { steamID = Client:GetUserId(), priority = alienPlaceInQueue, plyr = player }
+        self.alienQueue[ alienPlaceInQueue ] = { steamID = Client:GetUserId(), priority = alienPlaceInQueue, plyr = player:GetClient() }
         alienPlaceInQueue = alienPlaceInQueue + 1
       return false
       --else self:findNextPriority(newteam)
       end
 ------------------------------------------------------------------------------------------------------------------------------
     elseif newteam == 1 then
-    local MarineCount = gamerules:GetTeam1():GetNumPlayers()
+
         if MarineCount >=12 then
         Shine:NotifyDualColour( player, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "Marine Team Capped at 12 players" )
         Shine:NotifyDualColour( player, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "You've entered the Marine Team Queue at number " .. marinePlaceInQueue ) ---Great, what pos in queueue are we?
         self:RemoveFromOtherQueue(player, newteam)
-        self.marineQueue[ marinePlaceInQueue ] = { steamID = Client:GetUserId(), priority = marinePlaceInQueue, plyr = player }
+        self.marineQueue[ marinePlaceInQueue ] = { steamID = Client:GetUserId(), priority = marinePlaceInQueue, plyr = player:GetClient() }
         marinePlaceInQueue = marinePlaceInQueue + 1
         return false
        -- else self:findNextPriority(newteam)
         end
 ------------------------------------------------------------------------------------------------------------------------------
     elseif newteam == 3 then
-    local SpectCount = gamerules:GetTeam3():GetNumPlayers()
+
          if SpectCount >=6 then
         Shine:NotifyDualColour( player, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "Spectator Team Capped at 12 players" )
         Shine:NotifyDualColour( player, 0, 255, 0, "[Proving Ground]", 255, 255, 255, "You've entered the Spectator Team Queue at number " ..  spectPlaceInQueue ) ---Great, what pos in queueue are we?
         self:RemoveFromOtherQueue(player, newteam)
-        self.spectQueue[ spectPlaceInQueue ] = { steamID = Client:GetUserId(), priority = spectPlaceInQueue, plyr = player }
+        self.spectQueue[ spectPlaceInQueue ] = { steamID = Client:GetUserId(), priority = spectPlaceInQueue, plyr = player:GetClient() }
         spectPlaceInQueue = spectPlaceInQueue + 1
         return false
       -- else self:findNextPriority(newteam)
@@ -224,19 +237,19 @@ end
 function Plugin:ClientDisconnect(Client)
     local controlling = Client:GetControllingPlayer()
     if controlling:GetTeam() == 1 then
-            if self.marineQueue[ Client ]  then
+            if self.marineQueue[ marinePlaceInQueue ].steamID ~= -1  then
             marinePlaceInQueue = marinePlaceInQueue - 1
             end
             self:findNextPriority(1)
 ------------------------------------------------------------------------------------------------------------------------------
     elseif controlling:GetTeam() == 2 then
-            if self.alienQueue[ Client ]  then
+            if self.alienQueue[ alienPlaceInQueue ].steamID ~= -1  then
             alienPlaceInQueue = alienPlaceInQueue - 1
             end
             self:findNextPriority(2)
 ------------------------------------------------------------------------------------------------------------------------------
     elseif controlling:GetTeam() == 3 then
-            if self.spectQueue[ Client ]  then
+            if self.spectQueue[ spectPlaceInQueue ].steamID ~= -1  then
             spectPlaceInQueue = spectPlaceInQueue - 1
             end
             self:findNextPriority(3)
